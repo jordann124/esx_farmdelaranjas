38cc82f2325ce8f52f4f1b56663d24331af55254
Jordan <51269322+jordann124@users.noreply.github.com>
2019-06-03 20:15:43 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,20 @@
+Config              = {}
+Config.MarkerType   = 1
+Config.DrawDistance = 100.0
+Config.ZoneSize     = {x = 5.0, y = 5.0, z = 3.0}
+Config.MarkerColor  = {r = 0, g = 0, b = 0}
+Config.ShowBlips   = true  -- Blip Mapa
+
+Config.RequiredCopsKoda  = 0
+
+Config.TimeToFarm    = 1 * 1000
+Config.TimeToProcess = 1 * 1000
+Config.TimeToSell    = 1  * 1000
+
+Config.Locale = 'en'
+
+Config.Zones = {
+	ApanhaLaranjas =		{x = 328.64,	y = 6530.87,	z = 28.69,	name = _U('apanha_de_laranjas'),		sprite = 238,	color = 47},
+	SumoDeLaranja =	{x = 406.1,	y = 6450.72,	z = 28.81,	name = _U('transforma_do_sumo_de_laranja'),	sprite = 499,	color = 47},
+	VendaDeSumo =		{x = 104.73,	y = -932.8,	z = 29.82,	name = _U('venda_de_sumo_de_laranja'),		sprite = 500,	color = 47}
+}
---
+++ esx_farmdelaranjas.sql
@@ -0,0 +1,4 @@
+INSERT INTO `items` (name, label, `limit`) VALUES
+	('laranja', 'Laranjas', 50),
+	('sumo-de_laranja', 'Sumo De Laranja', 10)
+;
